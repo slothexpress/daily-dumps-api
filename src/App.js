6f0f0ccfbb6ba2c31f4bff8ba6a5e497b44e69e6
slothexpress <hellosami@protonmail.com>
2023-03-09 08:48:06 +0100

    Adjust starter files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const img = "https://www.tronalddump.io/img/tronalddump_850x850.png";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={img} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
