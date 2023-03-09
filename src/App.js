7355969e6fc4a0d90cda9d82ecf67522cc272020
slothexpress <hellosami@protonmail.com>
2023-03-09 10:48:11 +0100

    Add footer for cred to api server

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={img} className="App-logo" alt="logo" />
-
-        <Card />
-
       </header>
 
+      <main className="App-body">
+        <Card />
+        <Card />
+        <Card />
+      </main>
+
+      <footer className="App-footer">
+        <a href="https://www.tronalddump.io/" target="_blank">
+        powered by tronald dump API
+        </a>
+      </footer>
 
 
     </div>
